edaaee0103d9f1de0cd6f4fc577d8b944a2bed81
Otto303 <ottodebrie@gmail.com>
2026-04-27 23:31:46 +0200

    add: reload i3 after install + add: wallpapers

--- install.sh
+++ install.sh
@@ -57,4 +57,6 @@ sudo cp $PWD/greetd/config.toml /etc/greetd/config.toml
 echo -e '\e[0;1mGreeter changed\e[0;0m'
 
 # Done.
+i3-msg restart > /dev/null
+feh --bg-fill $HOME/Pictures/Wallpapers/nyarch.jpg
 echo -e "\e[0;1mDone.\e[0;0m"
