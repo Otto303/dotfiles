f587dec4aebc9f7df0703bc028374e191dc64d71
Otto303 <ottodebrie@gmail.com>
2026-06-09 15:04:55 +0200

    update vim config

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -13,7 +13,7 @@ return {
                 return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
             end
             -- Apply default mappings
-            api.config.mappings.default_on_attach(bufnr)
+            api.map.on_attach.default(bufnr)
             -- Override/Remove the default <C-t> behavior in the tree
             vim.keymap.set("n", "<C-t>", api.tree.toggle, opts("Toggle"))
         end
@@ -21,6 +21,12 @@ return {
         require("nvim-tree").setup({
             view = {
                 side = "right",
+            },
+            git = {
+                enable = true,
+                ignore = true,       -- Do not show files listed in gitignore
+                show_on_dirs = true, -- Shows git status icons on parent directories
+                timeout = 400,
             },
             diagnostics = {
                 enable = true,
@@ -33,7 +39,24 @@ return {
                 },
             },
             renderer = {
+                highlight_git = "name",
                 highlight_diagnostics = "name",
+                icons = {
+                    glyphs = {
+                        git = {
+                            unstaged  = " ",
+                            staged    = " ",
+                            unmerged  = " ",
+                            renamed   = "➜ ",
+                            untracked = " ",
+                            deleted   = "󰆴 ",
+                            ignored   = " ",
+                        },
+                    },
+                },
+            },
+            filters = {
+                dotfiles = true,
             },
             on_attach = my_on_attach,
         })
